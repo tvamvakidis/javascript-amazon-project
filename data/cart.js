cfa82256f43f01bb0b9102b168ed6032a9cc966a
Theodoros Vamvakidis <tvamvakidis@ots.gr>
2025-12-22 11:43:38 +0200

    Fix empty cart default value

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
+if (!cart || cart.length === 0) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
